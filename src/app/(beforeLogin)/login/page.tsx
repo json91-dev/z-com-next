87310edc3f0935f633b5f13a3bc475027a9a8c56
jungwoole91 <jungwoole91@gmail.com>
2024-01-08 00:02:56 +0900

    [feat] 1-11 로그인 모달에서 발생하는 문제 해결하기(router.replace)

--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -1,6 +1,16 @@
- import {redirect} from "next/navigation";
+'use client';
 
+import { useRouter } from 'next/navigation';
+import Main from '@/app/(beforeLogin)/_component/Main';
 
 export default function Login() {
-  redirect('/i/flow/login')
+  const router = useRouter();
+  router.replace('/i/flow/login');
+  return <Main />;
 }
+
+// router.push
+// localhost:3001 -> localhost:3001/login -> localhost:3001/i/flow/login
+
+// router.replace
+// localhost:3001 -> localhost:3001/i/flow/login
