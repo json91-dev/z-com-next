a108cbf99cf7bce428a45d09628f812cd159d847
jungwoole91 <jungwoole91@gmail.com>
2024-01-07 20:20:21 +0900

    [feat] 1-6 패러렐 라우트

--- src/app/(afterLogin)/home/layout.tsx
+++ src/app/(afterLogin)/home/layout.tsx
@@ -1,5 +1,11 @@
-export default async function HomeLayout({children}: any) {
-  return(
+import { ReactNode } from 'react';
+
+export default async function HomeLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
     <div>
       홈 레이아웃
       {children}
