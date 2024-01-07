01f166490f2a1f474e9ceb8a7d95d28dbf19d286
jjjjjw910911 <jjjjjw910911@letsee.io>
2024-01-07 16:42:49 +0900

    [1-4] template, tsx, Link, Image, redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+import Link from "next/link";
+
+
 export default function Home() {
   return (
-    <main >
-      메인페이지
-    </main>
+    <div>
+      <div className={styles.left}>
+        <Image src={zLogo} alt="logo" />
+      </div>
+
+      <div className={styles.right}>
+        <h1>지금 일어나고 있는 일</h1>
+        <h2>지금 가입하세요.</h2>
+        <Link href="/i/flow/signup" className={styles.signup}>계정 만들기</Link>
+        <Link href="login" className={styles.login}>로그인</Link>
+      </div>
+    </div>
   )
 }
