0db3a48e6995f5280c609d4fff924700e61148c6
jjjjjw910911 <jjjjjw910911@letsee.io>
2024-01-19 03:08:04 +0900

    [feat] 3-1

--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -1,16 +1,20 @@
-'use client';
+"use client";
 
-import { useRouter } from 'next/navigation';
-import Main from '@/app/(beforeLogin)/_component/Main';
+import {useRouter} from "next/navigation";
+import Main from "@/app/(beforeLogin)/_component/Main";
+import {useSession} from "next-auth/react";
 
 export default function Login() {
   const router = useRouter();
+  const { data: session } = useSession();
+
+  if (session?.user) {
+    router.replace('/home');
+    return null;
+  }
+
   router.replace('/i/flow/login');
-  return <Main />;
+  return (
+    <Main/>
+  );
 }
-
-// router.push
-// localhost:3001 -> localhost:3001/login -> localhost:3001/i/flow/login
-
-// router.replace
-// localhost:3001 -> localhost:3001/i/flow/login
